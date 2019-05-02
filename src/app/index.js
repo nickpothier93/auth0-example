911469207e52710c7ca2f35ae3c87038817ec03e
Nini Gondeh <ninigondeh@Mehrnazs-MacBook-Pro-2.local>
2019-05-02 16:29:04 -0400

    TaskOrganizer: Redux + React Skelton created to modify and create tasks

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
-console.log("Hello World");
-
 import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components/Main";
 
-console.log(store.getState());
+ReactDOM.render(<Main />, document.getElementById("app"));
